b19a01fa38a22114268fa2120e016240b9c8cefd
lewis-andy <lewisandanje3@gmail.com>
2023-11-15 16:21:52 +0300

    correction to the upadated background

--- login/script.js
+++ login/script.js
@@ -4,10 +4,10 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
 	var username = document.getElementById("username").value;
 	var password = document.getElementById("password-field").value;
 
-	if (username === "user@gmail.com" && password === "user123!") {
-		window.location.href = "online_retail_system/Frontend_RetailSysX/index.html"; // Redirect to index.html for user
-	} else if (username === "admin@gmail.com" && password === "admin123!") {
-		window.location.href = "online_retail_system/Frontend_RetailSysX/admin_panel/index.html"; // Redirect to admin.html for admin
+	if (username === "user" && password === "12345@") {
+		window.location.href = "user-interface/home.html"; // Redirect to home.html for user
+	} else if (username === "admin" && password === "12345@") {
+		window.location.href = "admin-panel/admin.html"; // Redirect to admin.html for admin
 	} else {
 		alert("Invalid username or password. Please try again."); // Show an error message for incorrect credentials
 	}
