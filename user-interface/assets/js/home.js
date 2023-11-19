143cc93a96d1338ab605b1e7026b4a8aa3f8fd28
lewis-andy <lewisandanje3@gmail.com>
2023-11-19 15:57:17 +0300

    an addition

---
+++ user-interface/assets/js/home.js
@@ -0,0 +1,4 @@
+document.getElementById('signOutIcon').addEventListener('click', function() {
+    // Perform sign-out action here
+    alert('Signing out...'); // Replace this with your actual sign-out logic
+  });
